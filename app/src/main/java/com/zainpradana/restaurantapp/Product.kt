a61cf91d89ac6a02332b9b17b793f59016887cc1
nurzainpradana <pradananurzain@gmail.com>
2020-11-12 13:28:51 +0700

    add plugin kotlin-android-extension and add arguments

--- app/src/main/java/com/zainpradana/restaurantapp/Product.kt
+++ app/src/main/java/com/zainpradana/restaurantapp/Product.kt
@@ -1,10 +1,14 @@
 package com.zainpradana.restaurantapp
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Product (
     var namaMenu: String = "",
     var hargaMenu: Int = 0,
     var jumlahMenu: Int = 0
-) {
+) : Parcelable {
     val subtotal: Int
         get() = jumlahMenu * hargaMenu
 }
