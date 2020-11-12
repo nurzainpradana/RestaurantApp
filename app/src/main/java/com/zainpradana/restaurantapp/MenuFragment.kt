9e7a48cdcd58370199132a5df175d8e17a61afb5
nurzainpradana <pradananurzain@gmail.com>
2020-11-12 14:45:57 +0700

    add args when navigate to HitungFragment

--- app/src/main/java/com/zainpradana/restaurantapp/MenuFragment.kt
+++ app/src/main/java/com/zainpradana/restaurantapp/MenuFragment.kt
@@ -6,22 +6,52 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.zainpradana.restaurantapp.databinding.FragmentMenuBinding
 
 class MenuFragment : Fragment() {
+
     private lateinit var binding: FragmentMenuBinding
+    lateinit var productSatu: Product
+    lateinit var productDua: Product
+    lateinit var productTiga: Product
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate<FragmentMenuBinding>(inflater, R.layout.fragment_menu, container, false)
+        binding.apply {
+            binding = DataBindingUtil.inflate(inflater, R.layout.fragment_menu, container, false)
 
-        val args = MenuFragmentArgs.fromBundle(requireArguments())
-        binding.nomorMeja = args.nomorMeja
+            val args = MenuFragmentArgs.fromBundle(requireArguments())
+            nomorMeja = args.nomorMeja
 
+            btnHitung.setOnClickListener { view : View ->
+                ambilData(binding)
+                view.findNavController().navigate(MenuFragmentDirections.actionMenuFragmentToHitungFragment(productSatu, productDua, productTiga, nomorMeja))
+            }
+            return binding.root
+        }
+    }
 
-        return binding.root
+    private fun ambilData(binding: FragmentMenuBinding) {
+        binding.apply {
+            if (etNamaMenuSatu.text.isNotEmpty() || etJumlahMenuSatu.text.isNotEmpty() || etHargaMenuSatu.text.isNotEmpty()) {
+                productSatu.namaMenu = etNamaMenuSatu.text.toString()
+                productSatu.hargaMenu = etHargaMenuSatu.text.toString().toInt()
+                productSatu.jumlahMenu = etJumlahMenuSatu.text.toString().toInt()
+            }
+            if (etNamaMenuDua.text.isNotEmpty() || etJumlahMenuDua.text.isNotEmpty() || etHargaMenuDua.text.isNotEmpty()) {
+                productDua.namaMenu = etNamaMenuDua.text.toString()
+                productDua.hargaMenu = etHargaMenuDua.text.toString().toInt()
+                productDua.jumlahMenu = etJumlahMenuDua.text.toString().toInt()
+            }
+            if (etNamaMenuTiga.text.isNotEmpty() || etJumlahMenuTiga.text.isNotEmpty() || etHargaMenuTiga.text.isNotEmpty()) {
+                productTiga.namaMenu = etNamaMenuTiga.text.toString()
+                productTiga.hargaMenu = etHargaMenuTiga.text.toString().toInt()
+                productTiga.jumlahMenu = etJumlahMenuTiga.text.toString().toInt()
+            }
+        }
     }
 }
